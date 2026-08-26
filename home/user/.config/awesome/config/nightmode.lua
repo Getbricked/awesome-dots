ad9380121eef4d05ea49dc06a6603abc8f0ba6dd
Clownki <92321444+Getbricked@users.noreply.github.com>
2026-08-26 12:10:22 +0700

    adjustment: move .nightmode to cache

--- home/user/.config/awesome/config/nightmode.lua
+++ home/user/.config/awesome/config/nightmode.lua
@@ -1,10 +1,17 @@
 local M = {}
 
-local file = os.getenv("HOME") .. "/.config/awesome/config/.nightmode"
+local cache_dir = os.getenv("HOME") .. "/.cache/awesome"
+local file = cache_dir .. "/nightmode"
+
+local function ensure_dir()
+	os.execute("mkdir -p " .. cache_dir)
+end
 
 function M.is_on()
 	local f = io.open(file, "r")
 	if not f then
+		ensure_dir()
+		M.set(false)
 		return false
 	end
 	local content = f:read("*all")
@@ -13,6 +20,7 @@ function M.is_on()
 end
 
 function M.set(on)
+	ensure_dir()
 	local f = io.open(file, "w")
 	if not f then
 		return
